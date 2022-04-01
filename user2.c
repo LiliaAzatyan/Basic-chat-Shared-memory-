9bfa0599d63209e79991fb2a669eca043744e365
LiliaAzatyan <88790616+LiliaAzatyan@users.noreply.github.com>
2022-04-01 23:32:08 +0400

    Update user2.c

--- user2.c
+++ user2.c
@@ -6,23 +6,27 @@
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 
 
 #define Ready 1
 #define NotReady -1
+#define SIZE 100
   
 struct Memory {
-    char buffer[100];
+    char buffer[SIZE];
     int state, pid1, pid2;
 };
   
 struct Memory* shm_ptr; 
-//  print message received from user1 
+
 void handler(int signum)
 {
-    // if signum == SIGUSR2, then user 2 received a message from user1
+    
     if (signum == SIGUSR2) {
-        printf("Received From User1: ");
+        printf("Received Message From User1: ");
         puts(shm_ptr->buffer);
     }
 }
@@ -32,13 +36,18 @@ int main()
     // pid of user2
     int pid = getpid();
     int shm_id;
-    //shared memory key value 
-    int key = 12345;
+    const char* shm_name = "Memory";
+  
     //creating shared memory 
-    shm_id = shmget(key, sizeof(struct Memory), IPC_CREAT | 0666);
+    shm_id = shm_open(shm_name, O_CREAT | O_RDWR, 0666);
   
     // attaching the shared memory
-    shm_ptr = (struct Memory*)shmat(shm_id, NULL, 0);
+    shm_ptr = (struct Memory*)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_id, 0);
+  if(shm_ptr == (void*)-1)
+	{
+		perror("Could not attach shared memory");
+		exit(1);
+	}
     // store the process id of user2 in shared memory
     shm_ptr->pid2 = pid;
     shm_ptr->state = NotReady;
@@ -58,7 +67,11 @@ int main()
         while (shm_ptr->state == Ready)
             continue;
     }
-  
-    shmdt((void*)shm_ptr);
+    if(shm_unlink(shm_name) != 0)
+	{
+		perror("Could not detach");
+		exit(1);
+	}
+    //shmdt((void*)shm_ptr);
     return 0;
 }
